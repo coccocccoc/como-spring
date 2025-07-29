52dfede6220b4c466e15a4dd74a73199a9e2b5e3
coccocccoc <wod5438@naver.com>
2025-07-29 18:06:18 +0900

    zcx

--- src/main/java/com/example/demo/studygroup/repository/StudyGroupRepository.java
+++ src/main/java/com/example/demo/studygroup/repository/StudyGroupRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.studygroup.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.studygroup.entity.StudyGroup;
 
 public interface StudyGroupRepository extends JpaRepository<StudyGroup, Integer> {
+	
 
 }
